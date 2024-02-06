d46d59565977c4ec6c61346e6aa8428b6aaa4db9
Esteban Pastor <estebanandrespastor@gmail.com>
2024-02-06 18:24:08 -0300

    feat: :sparkles: Add sign up form
    Now the user can see the ui for the user form

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,11 @@
-
+import SignUpForm from "../components/sign-up-form/SignUpForm";
 
 const SignIn = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="py-5">
+      <SignUpForm />
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
